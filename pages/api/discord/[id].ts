5ce8954fc97353d5668d78b36bafc22f18a39cdf
林通 <lintong320@gmail.com>
2022-02-14 21:46:34 +0800

    feat: add sesh api

---
+++ pages/api/discord/[id].ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { request } from 'lib/request/AxiosHelper'
+type Data = {
+  organization: {
+    task: { id: string; status: 'BACKLOG' | 'TODO' | 'IN_PROGRESS' | 'DONE' }[]
+  }
+}
+// 941665725112782868
+const fetchDiscordData = async (channelId: string) => {
+  return request({
+    url: `https://discord.com/api/guilds/${channelId}?with_counts=true`,
+    method: 'get',
+    headers: {
+      Authorization:
+        'Bot OTM5NTIxODE5MzEwOTYwNzMw.Yf6D4Q.lyvBMOiHrN5YV4GSqPIbaYJHbbk',
+    },
+  })
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { id } = req.query
+  if (typeof id === 'string') {
+    const data = await fetchDiscordData(id)
+    res.status(200).json(data)
+  }
+
+  res.status(500)
+}
---
+++ pages/api/sesh/[id].ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { request } from 'lib/request/AxiosHelper'
+type Data = {
+  rsvp_options: unknown
+  start_time: string
+  end_time: string
+}[]
+// 941665725112782868
+const fetchCalendarEvents = async (orgId: string) => {
+  return request({
+    url: `https://sesh.fyi/api/get_event_listings`,
+    method: 'post',
+    payload: {
+      access_token: '9xbD4YkcTJ22SQZy55CJzMfncII0X6',
+      token_type: 'Bearer',
+      guild_id: '678414857510453309',
+    },
+  }) as Promise<{ props: Data }>
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { id } = req.query
+  if (typeof id === 'string') {
+    const data = await fetchCalendarEvents(id)
+    res.status(200).json(data.props)
+  }
+
+  res.status(500)
+}
