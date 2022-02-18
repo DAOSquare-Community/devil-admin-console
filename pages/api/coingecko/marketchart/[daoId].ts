66776a4a183bf354708531867a99b7a2b5f7f0c3
LxzLLL <minepop09@163.com>
2022-02-18 16:10:32 +0800

    add coingecko market chart api

---
+++ pages/api/coingecko/marketchart/[daoId].ts
@@ -0,0 +1,41 @@
+// get daosquare's price,market totalsupply
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
+import { format } from 'util'
+import tunnel from 'tunnel'
+
+type Data = {
+  daoId: string
+  prices: number[]
+  total_volumes: number[]
+}
+const url_coingecko_marketchart =
+  'https://api.coingecko.com/api/v3/coins/%s/market_chart?vs_currency=usd&days=max&interval=daily'
+
+// daosquare
+const fetchCoinGeckoData = async (daoId: string) => {
+  return axios
+    .get(format(url_coingecko_marketchart, daoId))
+    .then((response) => response.data)
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { daoId } = req.query
+  if (typeof daoId === 'string') {
+    const fetchData = await fetchCoinGeckoData(daoId)
+    if (!!fetchData) {
+      const retData: Data = {
+        daoId: daoId,
+        prices: fetchData.prices,
+        total_volumes: fetchData.total_volumes,
+      }
+      res.status(200).json(retData)
+    }
+  }
+  res.status(500)
+}
