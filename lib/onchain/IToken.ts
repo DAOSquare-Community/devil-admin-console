8615452db61febc967037a8172f96f29dad78743
LxzLLL <minepop09@163.com>
2022-05-22 13:26:42 +0800

    add   dao‘s  holders

--- lib/onchain/IToken.ts
+++ lib/onchain/IToken.ts
@@ -2,6 +2,7 @@ import { HolderTokens } from './holdertokens'
 
 interface IToken {
   getHolderTokensByAddress(addr: string): Promise<HolderTokens | null>
+  getHoldersCount(addr: string): Promise<number>
 }
 
 export default IToken
